611d38fda87f31fd13a1f94ad06c2d747fae8e17
819605834 <124411012+819605834@users.noreply.github.com>
2023-05-01 15:15:30 -0700

    Update BinaryTree.h
    function

--- BinaryTree.h
+++ BinaryTree.h
@@ -6,7 +6,15 @@ namespace bst
 	template <typename T>
 	class BinaryTree
 	{
-
+		public:
+			BinaryTree(Node root = NULL);
+			void PrintAll() const; // print all the node in the bin
+			bool AddNode(T newVal) const;// would be Node or a type of data
+			bool Find(T data) const;// sreach node, return true if find else return false
+			bool Compare(BinaryTree tree);//compare with another binary tree
+			void Clear() const;//empty the tree
+		private:
+			Node root;
 	};
 }
 
