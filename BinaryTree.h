dee6ff2845dfce96b737b8c8fae9e749fe8533ff
JohnSmoit <54293281+JohnSmoit@users.noreply.github.com>
2023-05-01 20:45:31 -0700

    Flesh out Node Class

--- BinaryTree.h
+++ BinaryTree.h
@@ -1,12 +1,26 @@
 #pragma once
 #include "Node.h"
 
+
 namespace bst 
 {
-	template <typename T>
-	class BinaryTree
-	{
+	template <typename T, typename Compare = GenericComparison<T>>
+	class BinaryTree {
+		public:
+			BinaryTree(Node<T, Compare> root = NULL);
+			void insertNode(T data);
+			void deleteNode(T data);
+			void clear() const;
+			bool find() const;
+			bool isBalanced();
 
+			Node<T, Compare>* NewNode(T& data, Node<T, Compare>* parent = nullptr, Node<T, Compare>* left = nullptr, Node<T, Compare>* right = nullptr); //uses less template mess this way.
+		protected:
+			Node<T, Compare>* insertNode(T data, Node<T, Compare>* node);
+			Node<T, Compare>* deleteNode(T data, Node<T, Compare>* node);
+			bool isBalanced(Node<T, Compare>* node);
+		private:
+			Node<T, Compare>* root;
 	};
 }
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <string>
 #include "BinaryTree.h"
 
+struct StringComp 
+{
+	bool compare(std::string A, std::string B) const 
+	{
+		return A == B;
+	}
+};
 
-
-int main () 
+int main() 
 {
 	std::cout << "Hello World!" << std::endl;
+
+	bst::Node<std::string, StringComp> node("Hello World");
+
+	std::cout << node.data() << std::endl;
+
+	node.data() = "Goodbye World!";
+
+	std::cout << node.data() << std::endl;
+
 	return 0;
 }
